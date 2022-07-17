c7fed0a19401c14e6a312c9729424c87b87039bc
Sven Lüchow <sven.luechow@icloud.com>
2022-07-17 15:03:40 +0200

    :recycle: Build hook for passwordcheck
    Build hook for passwordcheck

--- src/hooks/usePasswordCheck.tsx
+++ src/hooks/usePasswordCheck.tsx
@@ -3,6 +3,10 @@ import { Password } from "../models/Password";
 
 export default function usePasswordCheck(password: Password) {
   const firstPasswordLength = password.firstPassword.length;
+  const [showFirstPassword, setShowFirstPassword] = useState(false);
+  const [showSecondPassword, setShowSecondPassword] = useState(false);
+  const [infoMessage, setInfoMessage] = useState("");
+  const [isValidPassword, setIsValidPassword] = useState(false);
   const [validLength, setValidLength] = useState(false);
   const [hasNumber, setHasNumber] = useState(false);
   const [upperCase, setUpperCase] = useState(false);
@@ -20,7 +24,7 @@ export default function usePasswordCheck(password: Password) {
     setSpecialChar(/[ `!@#$%^&*()_+\-=\]{};':"\\|,.<>?~]/.test(password.firstPassword));
   }, [password, requiredLength, firstPasswordLength]);
 
-  function checkPasswordValidation(value: string): string | boolean {
+  function checkPasswordValidation(value: string): void {
     const isWhitespace = /^(?=.*\s)/;
     const isContainsUppercase = /^(?=.*[A-Z])/;
     const isContainsLowercase = /^(?=.*[a-z])/;
@@ -29,26 +33,50 @@ export default function usePasswordCheck(password: Password) {
     const isValidLength = /^.{10,}$/;
 
     if (isWhitespace.test(value)) {
-      return "Password must not contain Whitespaces.";
+      setInfoMessage("Password must not contain whitespaces.");
+      setIsValidPassword(false);
+      return;
     }
     if (!isContainsUppercase.test(value)) {
-      return "Password must have at least one Uppercase Character.";
+      setInfoMessage("Password must have at least one uppercase character.");
+      setIsValidPassword(false);
+      return;
     }
     if (!isContainsLowercase.test(value)) {
-      return "Password must have at least one Lowercase Character.";
+      setInfoMessage("Password must have at least one lowercase character.");
+      setIsValidPassword(false);
+      return;
     }
     if (!isContainsNumber.test(value)) {
-      return "Password must contain at least one Digit.";
+      setInfoMessage("Password must contain at least one digit.");
+      setIsValidPassword(false);
+      return;
     }
     if (!isContainsSymbol.test(value)) {
-      return "Password must contain at least one Special Symbol.";
+      setInfoMessage("Password must contain at least one special symbol.");
+      setIsValidPassword(false);
+      return;
     }
     if (!isValidLength.test(value)) {
-      return "Password must have more than 10 Characters.";
+      setInfoMessage("Password must have more than 10 characters.");
+      setIsValidPassword(false);
+      return;
     } else {
-      return "Good password ✅";
+      setInfoMessage("Good password ✅");
+      setIsValidPassword(true);
     }
   }
+
+  function handleShowFirstPassword(event: React.MouseEvent<HTMLButtonElement>) {
+    event.preventDefault();
+    setShowFirstPassword(!showFirstPassword);
+  }
+
+  function handleShowSecondPassword(event: React.MouseEvent<HTMLButtonElement>) {
+    event.preventDefault();
+    setShowSecondPassword(!showSecondPassword);
+  }
+
   return {
     checkPasswordValidation,
     validLength,
@@ -59,5 +87,11 @@ export default function usePasswordCheck(password: Password) {
     match,
     requiredLength,
     firstPasswordLength,
+    infoMessage,
+    isValidPassword,
+    handleShowFirstPassword,
+    handleShowSecondPassword,
+    showFirstPassword,
+    showSecondPassword,
   };
 }
