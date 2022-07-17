2d55202288537d35284bedeffbdfc620b42bd034
Sven Lüchow <sven.luechow@icloud.com>
2022-07-17 15:07:31 +0200

    :art: Create interfaces
    Create interfaces

---
+++ src/models/Checklist.ts
@@ -0,0 +1,9 @@
+export interface ChecklistProps {
+  validLength: boolean;
+  hasNumber: boolean;
+  upperCase: boolean;
+  lowerCase: boolean;
+  specialChar: boolean;
+  requiredLength: number;
+  firstPasswordLength: number;
+}
